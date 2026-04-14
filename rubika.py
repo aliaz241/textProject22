cd84065cbe2ff260d321242c77f967a3bc58f6a3
aliaz241 <79758500+aliaz241@users.noreply.github.com>
2026-04-14 13:05:49 +0330

    Update rubika.py

--- rubika.py
+++ rubika.py
@@ -1,1 +1,20 @@
+import requests
+import json
 
+def send_to_rubika(text):
+    with open("rubika_session.json") as f:
+        session = json.load(f)
+
+    url = "https://messengerg2c1.iranlms.ir/"
+
+    payload = {
+        "api_version": "5",
+        "method": "sendMessage",
+        "auth": session["auth"],
+        "data": {
+            "object_guid": "YOUR_GUID",
+            "text": text
+        }
+    }
+
+    requests.post(url, json=payload)
