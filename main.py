ea1ec3360e3e88f63c912de799d8168d27af8c78
aliaz241 <79758500+aliaz241@users.noreply.github.com>
2026-04-14 13:06:18 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from bot import fetch_messages
+from rubika import send_to_rubika
 
+def main():
+    messages = fetch_messages()
+
+    for m in messages:
+        send_to_rubika(m)
+
+if __name__ == "__main__":
+    main()
