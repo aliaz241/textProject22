b28a4e5ae12a4d25da87d35f6135d7158a712961
aliaz241 <79758500+aliaz241@users.noreply.github.com>
2026-04-14 13:05:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import requests
 
+BOT_TOKEN = "7810023171:AAGBtujYuGY0inhiSefujYOh2ONaPfLyb6w"
+
+def get_updates():
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/getUpdates"
+    res = requests.get(url).json()
+    return res.get("result", [])
+
+def fetch_messages():
+    updates = get_updates()
+    messages = []
+
+    for u in updates:
+        msg = u.get("channel_post")
+        if msg and "text" in msg:
+            messages.append(msg["text"])
+
+    return messages[-5:]
